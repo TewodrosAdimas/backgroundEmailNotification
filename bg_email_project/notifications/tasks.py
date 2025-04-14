dfc82f758f152bbd8f92ca71a0dadcda7c35b1a3
TewodrosAdimas <yosefenadagim@gmail.com>
2025-04-14 20:22:38 +0300

    error handled

--- bg_email_project/notifications/tasks.py
+++ bg_email_project/notifications/tasks.py
@@ -1,12 +1,19 @@
 from celery import shared_task
 from django.core.mail import send_mail
+import logging
+
+logger = logging.getLogger(__name__)
 
 @shared_task
 def send_welcome_email(user_email):
+    logger.info(f"📤 Sending email to {user_email}")
+    
     subject = 'Welcome to Our Service'
     message = 'Thanks for signing up. We are glad to have you!'
-    from_email = 'your_email@gmail.com'
+    from_email = 'yosefenadagim@gmail.com'
     recipient_list = [user_email]
 
-    send_mail(subject, message, from_email, recipient_list)
+    result = send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+    logger.info(f"✅ Email sent result: {result}")
+
     return f"Sent welcome email to {user_email}"
